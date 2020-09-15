32b333e081e7ddf91584670f4fb45704bf7aa446
hundredrab <sourab.jha@kgec.edu.in>
2020-09-15 13:49:52 +0530

    MOAR CODE

--- assignment1/HMM/hmm.py
+++ assignment1/HMM/hmm.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 
 import nltk
 import numpy as np
-import seaborn as sb
+import seaborn as sns
+import  matplotlib.pyplot as plt
 from nltk.corpus import brown
 from sklearn.model_selection import KFold
 
@@ -12,7 +13,7 @@ nltk.download('universal_tagset')
 
 ## -------------------------------------
 
-tags = set(w[1] for w in brown.tagged_words(tagset='universal')).union({'^'})
+tags = set(w[1] for w in brown.tagged_words(tagset='universal'))
 
 ##
 known_words = set(brown.words())
@@ -22,6 +23,7 @@ class HMM:
     def __init__(self):
         self.emissions = defaultdict(dict)
         self.transitions = defaultdict(dict)
+        self.tags = set(tags).union({'^'})
 
     def train(self, dataset):
         for sent in dataset:
@@ -46,6 +48,7 @@ class HMM:
                 continue
             self.emissions[tag]['<UNK>'] = 1
             for word in known_words:
+                word = word.lower()
                 if word not in self.emissions[tag].keys():
                     self.emissions[tag][word] = 1
                 else:
@@ -64,8 +67,8 @@ class HMM:
                 k: v/total for k, v in data.items()}
 
         ## Make sure there are self.transitions between all states
-        for tag1 in tags:
-            for tag2 in tags:
+        for tag1 in self.tags:
+            for tag2 in self.tags:
                 self.transitions[tag1]['^'] = 0  # Can't go back to ^
                 if tag2 not in self.transitions[tag1]:
                     self.transitions[tag1][tag2] = 0.00001
@@ -74,17 +77,17 @@ class HMM:
         """On given a tokenized sentence (list) returns a list of POS tokens."""
         sentence = [x if x in known_words else '<UNK>' for x in sentence]
         sentence.insert(0, '^')
-        matrix = [{tag: (0, ) for tag in tags} for _ in sentence]
+        matrix = [{tag: (0, ) for tag in self.tags} for _ in sentence]
         matrix[0]['^'] = (1, )
         for i, word in enumerate(sentence):
             word = word.lower()
             if i == 0:
                 continue
-            for curr in tags:
+            for curr in self.tags:
                 if word not in self.emissions[curr]:
                     self.emissions[curr][word] = 0
                 all_p = [(self.emissions[curr][word] * self.transitions[prev][curr]
-                          * matrix[i-1][prev][0], prev) for prev in tags]
+                          * matrix[i-1][prev][0], prev) for prev in self.tags]
                 matrix[i][curr] = max(all_p)
 
         test_pos = [max(matrix[-1], key=lambda key: matrix[-1][key])]
@@ -95,7 +98,7 @@ class HMM:
 
 ##
 # sentence = nltk.word_tokenize("He said that he will go soon")
-#tag_pos(sentence)
+# tag_pos(sentence)
 # hmm = HMM()
 # hmm.train(dataset)
 # hmm.tag_pos(sentence)
@@ -106,34 +109,59 @@ full_dataset = np.asarray(brown.tagged_sents(tagset='universal'))
 print(f"Total sents = {len(full_dataset)}")
 
 ##
+
+tags = sorted(list(tags))
+
+if '^' in tags:
+    tags.remove('^')
 tag_to_i = dict([(j,i) for (i, j) in enumerate(tags)])
-tags = list(tags)
+
 confusion = np.zeros((len(tags), len(tags)))
+predictions = []
+truths = []
 def calculate_accuracy(row):
     # print(row.shape)
     sent, ground = zip(*row[0])
     pred = model.tag_pos(sent)
+    predictions.extend(pred)
+    truths.extend(ground)
     for i,j in zip(ground, pred):
+        if i == '^' or j == '^':
+            continue
         confusion[tag_to_i[i]][tag_to_i[j]] += 1
-    return sum(x==y for x,y in zip(ground, pred))
+    ret = sum(x==y for x,y in zip(ground, pred))
+    if ret < len(sent)/2:
+        print("\t".join(sent))
+        print("\t".join(ground))
+        print("\t".join(pred))
+    return ret
 
 
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 for train_index, test_index in kf.split(full_dataset):
     train_set = full_dataset[train_index]
-    test_set = full_dataset[test_index][:3]
+    test_set = full_dataset[test_index]
     model = HMM()
     model.train(train_set)
-    print("trained", end="")
+    # print("trained", end="")
     ts = test_set.reshape(-1, 1)
     tr = train_set.reshape(-1, 1)
+
     # correct_train = sum(np.apply_along_axis(calculate_accuracy, 1, tr))
     # total_train = sum(np.apply_along_axis(lambda x: len(x[0]), 1, tr))
+    # print(f"Train acc: {100* correct_train/total_train}", end="\t")
 
     correct = sum(np.apply_along_axis(calculate_accuracy, 1, ts))
     total = sum(np.apply_along_axis(lambda x: len(x[0]), 1, ts))
     print(f"Test accuracy: {100 * correct/total}")
 ##
-confusion = confusion/confusion.sum(axis=0)
-confusion = np.nan_to_num(confusion)
-sb.heatmap(confusion, yticklabels=tags, xticklabels=tags)
+conf = (confusion.T/(confusion.sum(axis=1))).T
+conf = np.nan_to_num(conf)
+
+plt.figure(figsize=(10, 8))
+ax = sns.heatmap(conf, annot=True, fmt=".1f", cmap="Reds",
+                 xticklabels=tags, yticklabels=tags)
+ax.set_ylim(12, 0)
+plt.show()
+
+##
